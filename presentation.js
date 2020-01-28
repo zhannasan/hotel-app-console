40106b2060e133d3b67e51a6fc0648e2ad392750
Zhanna <j.santybay@gmail.com>
2020-01-28 15:44:54 +0100

    TP - Menu 1

--- presentation.js
+++ presentation.js
@@ -1,25 +1,57 @@
 function start(){
     var readline = require('readline');
-
+    var service = require('./service.js');
 var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-console.log('1. Lister les clients\n99. Sortir');
-rl.question('Choisir une action : ...', function(saisie){
-    if(saisie==='1'){
-    console.log(`Liste des clients :`);
-    var clientModule = require('./service.js');
-    var listeClients = clientModule.getClients();
 
-}else if (saisie==='99') {
-    console.log(`Aurevoir!`);
-} else {
-    console.log(`Veuiilez choisir`)
+function showMenu(){
+    console.log(`
+    1. Lister les clients.
+    2. Ajouter un client.
+    3. Rechercher un client par nom.
+    99. Sortir
+    `);
 }
-    rl.close();
+showMenu(); 
+rl.question('Choisir une action : ... ', function(saisie){
+    switch(saisie){
+    case '1':
+        console.log(`Liste des clients :`);
+   
+        service.getClients(function(body){
+            body.forEach(function(client) {
+                console.log(client.nom+" "+client.prenoms);
+            });
+            showMenu();
+        })
+        break;
+    case '2':
+       rl.question(`Veuillez saisir le nom du client à ajouter :`, function(){
+
+        });
+        break;
+    case '3':  
+        /*rl.question(`Veuillez saisir le nom du client...`,function(choix3){
+
+        });*/
+        service.getClients(function(body){
+            body.forEach(function(client) {
+                console.log(client.nom+" "+client.prenoms);
+            });
+            showMenu();
+        })
+        break;
+    case '4':  
+    break;
+    case '99':
+        console.log(`Aurevoir!`);
+        rl.close();
+        break;
+    default:
+        break;
+    }
 })
-    
 }
 exports.start = start;
-
--- service.js
+++ service.js
@@ -1,11 +1,41 @@
-function getClients(){
-    var request = require('request');
+var request = require('request');
+function getClients(callback){
     request('https://spring-mvc-hotel-app.herokuapp.com/client/', {json : true}, 
         function(err, res,body){
             if(err){
-                return console.log('Erreur',err);
+                console.log('Erreur',err);
+                callback(error);  
             }
-            console.log('Ok', body);
+            callback(body);;
         });
 }
 exports.getClients = getClients;
+
+function addClient(callback){
+    request.post('https://spring-mvc-hotel-app.herokuapp.com/client/', {
+        json : true},
+          //  nom: 'nomClient',
+           // prenoms: 'prenomClient'}, 
+        function(err, res,body){
+            if(err){
+                console.log('Erreur',err);
+               return
+            }
+          
+            console.log(`statusCode: ${res.statusCode}`);
+            console.log(body)
+        });
+}
+//exports.addClients = addClients;
+
+function getClientByName(callback){
+    request('https://spring-mvc-hotel-app.herokuapp.com/client?nom=Pierre', {json : true}, 
+        function(err, res,body){
+            if(err){
+                console.log('Erreur',err);
+                callback(error);  
+            }
+            callback(body);
+        });
+}
+exports.getClientByName = getClientByName;
